b1c6e9a13bd3ca941b0b5c9c0ba7c40a8695ba2b
brianhsu7476 <brianhsu7476@gmail.com>
2024-08-19 07:04:56 +0800

    slightly change fontSize

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 var ww=window.innerWidth, wh=window.innerHeight;
 var a=document.getElementsByClassName('bton');
 var btonSize=Math.min(ww/6, wh/6), bdSize=Math.min(ww/45, wh/50);
-if(ww<=800)bdSize*=0.9;
+if(ww<=800)bdSize*=0.8;
 for(var i=0; i<a.length; ++i)a[i].style.width=a[i].style.height=btonSize, a[i].style.fontSize=bdSize;
 document.querySelector('#board').style.fontSize=bdSize;
